ae7d140b5861914009c7bc46137ccf7b8082880c
Aro-M <ar.militosyan.97@mail.ru>
2023-02-20 10:52:27 +0400

    modified_the_code_to_match_the_style_of_the_code

--- demopages/tests.py
+++ demopages/tests.py
@@ -1,10 +1,10 @@
-from django.test import TestCase , Client
+from django.test import TestCase, Client
 
 class IndexTestCase(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = Client()
 
     def test_get_index_endpoint(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(response.content,b'Hello, world')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello, world')
--- demopages/views.py
+++ demopages/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
+
 def index(response):
     return HttpResponse("Hello, world")
